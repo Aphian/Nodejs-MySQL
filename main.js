c2c34cd0244ef50584d9045a22571ffffd6e1b97
Sang <rsw7876@naver.com>
2025-02-19 22:36:15 +0900

    nodejs_mysql

--- main.js
+++ main.js
@@ -101,27 +101,33 @@ var app = http.createServer(function(request,response){
       )
     });
   } else if(pathname === '/update') {
-    fs.readdir('./data', function(error, filelist) {
-      var filteredId = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredId}`, 'utf-8', function(err, description){
-        var title = queryData.id;
-        var list = template.list(filelist);
-        var html = template.HTML(title, list,
+    // fs.readdir('./data', function(error, filelist) {
+    db.query(`SELECT * FROM topic`, function(error, topics){
+      if (error) {
+        throw error;
+      }
+      // fs.readFile(`data/${filteredId}`, 'utf-8', function(err, description){
+      db.query(`SELECT * FROM topic WHERE id = ?`,[queryData.id], function(error2, topic){
+        if (error2) {
+          throw error2;
+        }
+        var list = template.list(topics);
+        var html = template.HTML(topic[0].title, list,
           `
           <form action="/update_process" method="post">
-            <input type="hidden" name="id" value="${title}" />
+            <input type="hidden" name="id" value="${topic[0].id}" />
             <p>
-              <input type="text" name="title" placeholder="title" value="${title}"/>
+              <input type="text" name="title" placeholder="title" value="${topic[0].title}"/>
             </p>
             <p>
-              <textarea name="description" placeholder="description">${description}</textarea>
+              <textarea name="description" placeholder="description">${topic[0].description}</textarea>
             </p>
             <p>
               <input type="submit" value="Submit">
             </p>
           </form>
           `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
         );
         response.writeHead(200);
         response.end(html);
