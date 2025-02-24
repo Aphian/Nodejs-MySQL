a81508ac30811e4c6ffe587bc7ca4bc94c9c74be
Sang <rsw7876@naver.com>
2025-02-24 21:05:31 +0900

    TIL

--- main.js
+++ main.js
@@ -4,23 +4,16 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var db = require('./lib/db.js');
 
+// Contents
+var topic = require('./lib/topic.js');
+
 var app = http.createServer(function(request,response){
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
   if (pathname === '/') {
     if (queryData.id === undefined) {
-      db.query(`SELECT * FROM topic`, function(error, topics){
-        var title = 'Welcome';
-        var description = 'Hello, Node.js !!';
-        var list = template.list(topics);
-        var html = template.HTML(title, list, 
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
+      topic.home(request, response);
     } else {
       db.query(`SELECT * FROM topic`, function(error, topics){
         if (error) {
