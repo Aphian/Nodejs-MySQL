3a63b4181cd0bcc0eb1cb8da019033799947117a
Sang <rsw7876@naver.com>
2025-02-25 21:19:24 +0900

    nodejs_mysql

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var db = require('./lib/db.js');
-var athor = require('./lib/author.js');
+var author = require('./lib/author.js');
 
 // Contents
 var topic = require('./lib/topic.js');
 
 var app = http.createServer(function(request,response){
-  // var _url = request.url;
-  // var queryData = url.parse(_url, true).query;
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
   if (pathname === '/') {
     if (queryData.id === undefined) {
